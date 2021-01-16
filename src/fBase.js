91760b9082e543a48f8d1732a13445117338f39a
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-01-16 20:36:46 +0900

    s

--- src/fBase.js
+++ src/fBase.js
@@ -5,9 +5,10 @@ const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY ,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
     projectId: process.env.REACT_APP_PROJECT_ID ,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET ,
     messagingSenderId: process.env.REACT_APP_MESSAING_ID ,
-    appId: process.env.REACT_APP_APP_ID
+    appId: process.env.APP_APP_ID
 }
 firebase.initializeApp(firebaseConfig);
 
